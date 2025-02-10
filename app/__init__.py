d24a999e265caea98d3154c50abe201819f8b844
Kauan <kauanlucastoldo@gmail.com>
2025-02-10 19:24:17 -0300

    add routes register and login

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@
 from flask import Flask
 import os
 from .routes.home import home
-
-
+from .routes.register import register
+from .routes.login import login
 
 def createApp():
     app = Flask(__name__)
     app.register_blueprint(home)
+    app.register_blueprint(register)
+    app.register_blueprint(login)
     app.secret_key = os.environ.get('SECRET_KEY')
     return app
