42119aab9c213542fe5917e7eb36f9ca5f1a16c2
WilliamKunzler <wkwilliamkunzler@gmail.com>
2025-02-11 09:23:46 -0300

    update database

--- app/database/schema.sql
+++ app/database/schema.sql
@@ -1,31 +1,26 @@
 create database Finapp;
 use Finapp;
 
-
-CREATE TABLE registroUsers(
-	UserID int AUTO_INCREMENT primary key,
+create table usuarios(
+	UserID int auto_increment primary key,
     email varchar(100) not null,
     nome varchar(100) not null,
     senha varchar(100) not null
 );
 
-CREATE TABLE login (
-    LoginID INT AUTO_INCREMENT PRIMARY KEY,
-    UserID INT NOT NULL,
-    nome VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL,
-    FOREIGN KEY (UserID) REFERENCES RegistroUsers(UserID)
+create table entrada (
+    entradaID int auto_increment primary key,
+    entrada_data datetime not null,
+    categoria varchar(100) not null,
+    valor double(10,2) not null,
+    remetente varchar(100)
 );
 
-
-DELIMITER //
-CREATE TRIGGER `finapp`.`registrousers_BEFORE_DELETE` BEFORE DELETE ON `registrousers` FOR EACH ROW
-BEGIN
-	DELETE FROM Login WHERE UserID = OLD.UserID;
-END;
-//
-CREATE TRIGGER `finapp`.`registrousers_AFTER_INSERT` AFTER INSERT ON `registrousers` FOR EACH ROW
-BEGIN
-	INSERT INTO Login (UserID, nome, senha) VALUES (NEW.UserID, NEW.nome, NEW.senha);
-END;
-//
+create table saida (
+	saidaID int auto_increment primary key,
+    saida_data datetime not null,
+    categoria varchar(100) not null,
+    valor double(10,2) not null,
+    estado varchar(100) not null,
+    destinatario varchar(100)
+);
