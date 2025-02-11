67dc5ee141ac0b9ee0d6b63bc3e8415c2c6cb9dc
Kauan <kauanlucastoldo@gmail.com>
2025-02-11 10:07:46 -0300

    html sidebar

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,15 @@ import os
 from .routes.home import home
 from .routes.register import register
 from .routes.login import login
+from .routes.transactions import transactions
+from .routes.settings import settings
 
 def createApp():
     app = Flask(__name__)
     app.register_blueprint(home)
     app.register_blueprint(register)
     app.register_blueprint(login)
+    app.register_blueprint(transactions)
+    app.register_blueprint(settings)
     app.secret_key = os.environ.get('SECRET_KEY')
     return app
